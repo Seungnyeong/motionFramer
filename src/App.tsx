2d4e6a10ceb2c1da74f8cd17d5a4f4ccdcbebeab
김승녕 <workingsnkim@gmail.com>
2022-01-16 15:47:40 +0900

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,383 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+  AnimatePresence,
+} from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
+const Wrapper = styled(motion.div)`
+  height: 500vh;
+  width: auto;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+const BoxWraaper = styled.div`
+  display: flex;
+  width: auto;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  flex-direction: column;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Box2 = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
+`;
+
+const Circle = styled(motion.div)`
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  background-color: white;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
+`;
+
+const myVariants = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
+
+const box2Variant = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+const Box3 = styled(motion.div)`
+  width: 50px;
+  height: 50px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const box3Variants = {
+  hover: { scale: 1.5, rotateZ: 90 },
+  tap: { scale: 1, borderRadius: "100px" },
+  drag: {
+    backgroundColor: "rgba(52, 152, 219, 1.0)",
+    trasition: { duration: "10s" },
+  },
+};
+
+const BiggerBox = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const Box4 = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Svg = styled.svg`
+  width: 300px;
+  height: 300px;
+  path {
+    stroke: white;
+    stroke-width: 2;
+  }
+`;
+
+const svg = {
+  start: { pathLength: 0, fill: "rgba(255,255,255,0)" },
+  end: {
+    fill: "rgba(255,255,255, 1)",
+    pathLength: 1,
+  },
+};
+
+const Box5 = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Box6 = styled(motion.div)`
+  width: 50px;
+  height: 50px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  position: absolute;
+`;
+
+const Box5Variants = {
+  initial: {
+    opacity: 0,
+    sacle: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opactiy: 0,
+    y: 20,
+  },
+};
+
+const box6Variant = {
+  entry: (back: boolean) => ({
+    x: back ? -500 : 500,
+    opacity: 0,
+    scale: 0,
+  }),
+  center: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+  exit: (back: boolean) => ({
+    x: back ? 500 : -500,
+    opacity: 0,
+    scale: 0,
+    transition: {
+      duration: 1,
+    },
+  }),
+};
+
+const Circle2 = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Box7 = styled(motion.div)`
+  width: 400px;
+  height: 400px;
+  background-color: white;
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Grid = styled.div`
+  display: gird;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10;
+  div:first-child,
+  div:last_child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box8 = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 2]);
+  const [showing, setShowing] = useState(false);
+  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visiable, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const nexetPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
+
+  const [id, setId] = useState<null | string>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper style={{ background: gradient }}>
+      <Box variants={myVariants} initial="start" animate="end" />
+      <Box2 variants={box2Variant} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box2>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box3
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={box3Variants}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="tap"
+        />
+      </BiggerBox>
+      <Box4 style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
+      <Svg
+        focusable="false"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <motion.path
+          variants={svg}
+          initial="start"
+          animate="end"
+          transition={{
+            default: { duration: 5 },
+            fill: { duration: 2, delay: 3 },
+          }}
+          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+        ></motion.path>
+      </Svg>
+      <AnimatePresence>
+        {showing && (
+          <Box5
+            variants={Box5Variants}
+            initial="initial"
+            animate="visible"
+            exit="leaving"
+          ></Box5>
+        )}
+      </AnimatePresence>
+      <button onClick={toggleShowing}>Click</button>
+
+      <BoxWraaper>
+        <AnimatePresence custom={back}>
+          <Box6
+            custom={back}
+            variants={box6Variant}
+            initial="entry"
+            animate="center"
+            exit="exit"
+            key={visiable}
+          >
+            {visiable}
+          </Box6>
+        </AnimatePresence>
+        <button onClick={nexetPlease}>next</button>
+        <button onClick={prevPlease}>prev</button>
+      </BoxWraaper>
+      <BoxWraaper onClick={toggleClicked}>
+        <Box7>
+          {!clicked ? (
+            <Circle2 layoutId="circle" style={{ borderRadius: 50, scale: 2 }} />
+          ) : null}
+        </Box7>
+        <Box7>
+          {clicked ? (
+            <Circle2 layoutId="circle" style={{ borderRadius: 0 }} />
+          ) : null}
+        </Box7>
+      </BoxWraaper>
+      <BoxWraaper>
+        <Grid>
+          {["1", "2", "3", "4"].map((n) => (
+            <Box8 onClick={() => setId(n)} key={n} layoutId={n} />
+          ))}
+        </Grid>
+        <AnimatePresence>
+          {id && (
+            <Overlay
+              onClick={() => setId(null)}
+              initial={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+              animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+              exit={{ backgroundColor: "rgba(0, 0, 0)" }}
+            >
+              <Box8 layoutId={id} style={{ width: 500, height: 200 }} />
+            </Overlay>
+          )}
+        </AnimatePresence>
+      </BoxWraaper>
+    </Wrapper>
   );
 }
 
